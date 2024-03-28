8f565dff592c1d0b5931fa707b7b325f26a810e2
Richard Renyi-Vamos <82152266+richard-renyi-vamos@users.noreply.github.com>
2024-03-28 10:39:42 +0100

    Create PYTHON - TENSORFLOW - animated graphic prediction based on a dataset.py

---
+++ PYTHON - TENSORFLOW - animated graphic prediction based on a dataset.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Generate some sample data
+np.random.seed(0)
+X_train = np.linspace(0, 10, 100)
+y_train = 3 * X_train + 2 + np.random.randn(100) * 2
+
+# Define the linear regression model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=1, input_shape=[1])
+])
+
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# Train the model
+model.fit(X_train, y_train, epochs=100, verbose=0)
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 40)
+
+# Create empty line
+line, = ax.plot([], [], lw=2)
+
+# Initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+# Animation function: this is called sequentially
+def animate(i):
+    x = np.linspace(0, 10, 100)
+    y_pred = model.predict(x).flatten()
+    line.set_data(x, y_pred)
+    return line,
+
+# Create animation
+ani = FuncAnimation(fig, animate, frames=100, init_func=init, blit=True)
+
+plt.scatter(X_train, y_train)  # Plot the training data points
+plt.title('Animated Prediction')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.show()
